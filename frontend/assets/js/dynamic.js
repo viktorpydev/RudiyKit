cff38bc6eedd09f5eaf60dd4ebac2bd86618dd43
Victor Danylenko <viktorpydev@gmail.com>
2026-06-28 13:26:36 +0300

    Move price below title in property card

--- frontend/assets/js/dynamic.js
+++ frontend/assets/js/dynamic.js
@@ -26,11 +26,11 @@ function renderPropertyCard(prop, delay) {
             </button>
         </div>
         <div class="property-card__content">
+            <h3 class="property-card__title">${prop.title}</h3>
             <div class="prop-price-row">
                 <span class="prop-price">${prop.price}</span>
                 ${m2PriceHtml}
             </div>
-            <h3 class="property-card__title">${prop.title}</h3>
             <div class="prop-location"><svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"></path><circle cx="12" cy="10" r="3"></circle></svg> ${prop.location}</div>
             <div class="prop-specs">
                 ${specsHtml}
